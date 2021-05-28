956b096dbde29cdacc9f7c0ddfb61d001883ca24
Eric Burgher <ericburgher@Erics-Air.attlocal.net>
2021-05-28 14:57:35 -0500

    Eric's anagram solution

--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,12 @@
 # Don't forget to run the tests (and create some of your own)
 
-def anagrams_for(word, list_of_words):
+#def anagrams_for(word, list_of_words):
 		# your code here
+def anagrams_for(s1, s2):
+     
+	output = []
+	for x in range(len(s2)):
+    # the sorted strings are checked
+		if(sorted(s1) == sorted(s2[x])):
+			output.append(s2[x])
+	return output
