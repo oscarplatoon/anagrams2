33913bf7c0a4c27885f9a00833a9ed946e819430
RenoAverill <reno_averill@yahoo.com>
2021-05-28 14:55:13 -0600

    Reno's solution anagram2.js

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,3 +1,21 @@
 exports.anagramsFor = function(word, listOfWords) {
+  // sort the original word
+  let originalSortedWord = word.split('').sort().join('')
+  // declare an empty array to push results too
+  let result = []
+// loop through array of words and pick out which ones match up
+  listOfWords.forEach(word =>{
+    let sortedWord = word.split('').sort().join('')
+    // push result the matching word
+    if (originalSortedWord === sortedWord) {
+      result.push(word)
+    }
+  });
+// return result
+  return result
 
-};
+}
+
+
+
+
