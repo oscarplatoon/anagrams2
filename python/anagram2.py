c48c2969ac6dd3255fd363d33781ff0685c69e28
Smattonline <smattonline@gmail.com>
2021-05-28 16:26:56 -0700

    Musa's Anagram 2 Python Solution

--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,19 @@
 # Don't forget to run the tests (and create some of your own)
 
 def anagrams_for(word, list_of_words):
+
 		# your code here
+	result = []
+	word_chars = sorted(list(word))
+	for each_word in list_of_words:
+		list_word_chars = sorted(list(each_word))
+
+		if word_chars == list_word_chars:
+			result.append(each_word)
+
+	return result
+
+#list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
+
+#print(anagrams_for("threads", list_of_words))
+#print(anagrams_for("apple", list_of_words))
--- python/anagram2_spec.py
+++ python/anagram2_spec.py
@@ -1,8 +1,28 @@
 # Can you translate this driver code to unit tests?
 
-from anagram2 import anagrams_for 
+import unittest
+from anagram2 import anagrams_for
+
+class Anagram2TestCase(unittest.TestCase):
+    """Tests for armstrong_numbers.py"""
+
+    def test_for_thread(self):
+        '''Check if word have any Anagram'''
+        self.assertEqual(anagrams_for("threads", list_of_words), ["threads", "trashed", "hardest", "hatreds"])
+
+    def test_for_apple(self):
+        '''Check if word have any Anagram'''
+        self.assertEqual(anagrams_for("apple", list_of_words), [])
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
 
 list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
 
+'''
 print(anagrams_for("threads", list_of_words) == ["threads", "trashed", "hardest", "hatreds"])
 print(anagrams_for("apple", list_of_words) == [])
+'''
