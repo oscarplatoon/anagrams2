f89ec68034a2fc70f4958c751853379c41b8b5fe
jung341416 <jung341416@gmail.com>
2021-05-28 16:09:25 -0400

    second function and its testcase

--- js/anagram.js
+++ js/anagram.js
@@ -1,10 +1,14 @@
+const sameLengthArrays = require("./sameLengthArrays");
+
 // write down function that takes string word, and array of words
 const anagram = (word, listOfWords) => {
   //create empty array name result
-  let result = []; 
+  //create variables according to helper function
+  let result = [];
+  let sameLengthWords = sameLengthArrays(word, listOfWords); 
   // create two helper function 
     //one that check the length of words in array and if they have same length push into new array
-    //one that sort the array of words and check if the words are the same
+    //one that sort the same legnth array of words and check if the words are the same
     //create each test cases
     // return result;
   return result;
