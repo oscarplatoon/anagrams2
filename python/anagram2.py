9cd0e90e6a2e0b06328598486fb6a6dd87d6a5f6
jakewhite5 <jakewhite5@yahoo.com>
2021-05-31 22:59:10 -0700

    python working with unittesting, js not working with testing

--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,8 @@
 # Don't forget to run the tests (and create some of your own)
 
 def anagrams_for(word, list_of_words):
-		# your code here
+	return_words = []
+	for word2 in list_of_words:
+		if (sorted(word.lower().replace(' ','')) == sorted(word2.lower().replace(' ',''))):
+			return_words.append(word2)
+	return return_words
--- python/anagram2_spec.py
+++ python/anagram2_spec.py
@@ -1,8 +1,22 @@
 # Can you translate this driver code to unit tests?
 
-from anagram2 import anagrams_for 
+from anagram2 import anagrams_for
+import unittest
 
 list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
 
-print(anagrams_for("threads", list_of_words) == ["threads", "trashed", "hardest", "hatreds"])
-print(anagrams_for("apple", list_of_words) == [])
+class TestAnagram2(unittest.TestCase):
+
+    def test_for_ana(self):
+        self.assertEqual(anagrams_for('threads', list_of_words), ["threads", "trashed", "hardest", "hatreds"])
+
+    def test_for_false(self):
+        self.assertEqual(anagrams_for('apple', list_of_words), [])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# print(anagrams_for("threads", list_of_words) == ["threads", "trashed", "hardest", "hatreds"])
+# print(anagrams_for("apple", list_of_words) == [])
